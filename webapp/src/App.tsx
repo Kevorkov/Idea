04d686438e42620ed8206d7e9cbdce39b836aae2
Sam Kevorkov <kevorkovss@gmail.com>
2024-12-16 11:34:27 +0300

    Added react-router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TrpcProvider } from './lib/trpc'
 import { AllIdeasPage } from './pages/AllIdeasPage'
-
+import { ViewIdeaPage } from './pages/ViewIdeaPage'
+import { getAllIdeasRoute, getViewIdeaRoute } from './lib/rotes'
 
 export const App = () => {
-  return(
+  return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   )
 }
