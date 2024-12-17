c91c66c846d7359fe48e36ea67b04743d5ebb37d
Sam Kevorkov <kevorkovss@gmail.com>
2024-12-17 11:25:07 +0300

    Added scss and NewIdeaPage

---
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -0,0 +1,9 @@
+import css from './index.module.scss'
+
+export const NewIdeaPage = () => {
+  return (
+    <div>
+      <h1 className={css.title}>New idea will be here soon</h1>
+    </div>
+  )
+}
