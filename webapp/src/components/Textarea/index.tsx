184b543125e19cddbc59fee84ccc05d0649e22a6
Sam Kevorkov <kevorkovss@gmail.com>
2024-12-18 13:56:38 +0300

    Added formik

---
+++ webapp/src/components/Textarea/index.tsx
@@ -0,0 +1,17 @@
+export const Textarea = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
+  const value = formik.values[name]
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <label htmlFor={name}>{label}</label>
+      <br />
+      <textarea
+        onChange={(e) => {
+          void formik.setFieldValue(name, e.target.value)
+        }}
+        value={value}
+        name={name}
+        id={name}
+      />
+    </div>
+  )
+}
