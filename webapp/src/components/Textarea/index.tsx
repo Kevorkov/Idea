288dc621bd401b5c5b38b3686ef8a67a7867e2eb
Sam Kevorkov <kevorkovss@gmail.com>
2024-12-18 14:46:08 +0300

    Added zod to webapp

--- webapp/src/components/Textarea/index.tsx
+++ webapp/src/components/Textarea/index.tsx
@@ -1,5 +1,8 @@
 export const Textarea = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
   const value = formik.values[name]
+  const error = formik.errors[name] as string | undefined
+  const touched = formik.touched[name]
+
   return (
     <div style={{ marginBottom: 10 }}>
       <label htmlFor={name}>{label}</label>
@@ -8,10 +11,14 @@ export const Textarea = ({ name, label, formik }: { name: string; label: string;
         onChange={(e) => {
           void formik.setFieldValue(name, e.target.value)
         }}
+        onBlur={() => {
+          void formik.setFieldTouched(name)
+        }}
         value={value}
         name={name}
         id={name}
       />
+      {!!touched && !!error && <div style={{ color: 'red' }}>{error}</div>}
     </div>
   )
 }
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -2,6 +2,8 @@ import { useFormik } from 'formik'
 import { Segment } from '../../components/Segment'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
+import { z } from 'zod'
+import { withZodSchema } from 'formik-validator-zod'
 
 export const NewIdeaPage = () => {
   const formik = useFormik({
@@ -11,6 +13,17 @@ export const NewIdeaPage = () => {
       description: '',
       text: '',
     },
+    validate: withZodSchema(
+      z.object({
+        name: z.string().min(1),
+        nick: z
+          .string()
+          .min(1)
+          .regex(/^[a-z0-9-]+$/, 'Nick may contain only lowercase letters, numbers and dashes'),
+        description: z.string().min(1),
+        text: z.string().min(100, 'Text should be at least 100 characters long'),
+      })
+    ),
     onSubmit: (values) => {
       console.info('Submitted', values)
     },
@@ -28,6 +41,7 @@ export const NewIdeaPage = () => {
         <Input name="nick" label="Nick" formik={formik} />
         <Input name="description" label="Description" formik={formik} />
         <Textarea name="text" label="Text" formik={formik} />
+        {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
         <button type="submit">Create Idea</button>
       </form>
     </Segment>
