598838db54cfbf1adee450e12c5df215aaa589fe
Sam Kevorkov <kevorkovss@gmail.com>
2024-12-24 10:36:16 +0300

    Added signUp page to webapp

--- webapp/src/lib/routes.ts
+++ webapp/src/lib/routes.ts
@@ -9,3 +9,5 @@ export type ViewIdeaRouteParams = typeof viewIdeaRouteParams
 export const getViewIdeaRoute = ({ ideaNick }: ViewIdeaRouteParams) => `/ideas/${ideaNick}`
 
 export const getNewIdeasRoute = () => `/ideas/new`
+
+export const getSignUpRoute = () => '/sign-up'
